55790835f7d946c80fa431132e9ae1fab6c18d9b
yohang <yohan@giarel.li>
2017-10-06 11:00:48 +0200

    Added winner mgmt

--- src/App.js
+++ src/App.js
@@ -10,10 +10,11 @@ class App extends Component {
             [null, null, null],
         ],
         currentPlayer: 'X',
+        winner: null,
     };
 
     onCellClick(row, column) {
-        if (null !== this.state.grid[row][column]) {
+        if (null !== this.state.grid[row][column] || null !== this.state.winner) {
             return;
         }
 
@@ -23,7 +24,41 @@ class App extends Component {
         this.setState({
             grid: newGrid,
             currentPlayer: this.state.currentPlayer === 'X' ? 'O' : 'X',
+            winner: this.hasWon(newGrid),
         });
+    }
+
+    hasWon(grid) {
+        for (let i = 0; i < 3; i++) {
+            if (
+                (
+                    grid[i][0] === grid[i][1] && grid[i][1] === grid[i][1] &&
+                    grid[i][0] !== null && grid[i][1] !== null && grid[i][2] !== null
+                ) ||
+                (
+                    grid[0][i] === grid[1][i] && grid[1][i] === grid[2][i] &&
+                    grid[0][i] !== null && grid[1][i] !== null && grid[2][i] !== null
+                )
+            ) {
+                return this.state.currentPlayer;
+            }
+        }
+
+        if (
+            (
+                grid[0][0] === grid[1][1] && grid[1][1] === grid[2][2] &&
+                grid[0][0] !== null && grid[1][1] !== null && grid[2][2] !== null
+
+            ) ||
+            (
+                grid[0][2] === grid[1][1] && grid[1][1] === grid[2][0] &&
+                grid[0][2] !== null && grid[1][1] !== null && grid[2][0] !== null
+            )
+        ) {
+            return this.state.currentPlayer;
+        }
+
+        return null;
     }
 
     render() {
@@ -33,6 +68,11 @@ class App extends Component {
                     <img src={logo} className="App-logo" alt="logo"/>
                     <h1 className="App-title">Welcome to React</h1>
                 </header>
+                {
+                    this.state.winner !== null ?
+                        <h1>{this.state.winner} a gagné</h1> :
+                        null
+                }
                 <table>
                     <tbody>
                         <tr>
